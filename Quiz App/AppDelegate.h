7572a6cc821e0f37fae4f6464abba65c59f24d5c
SewerSnake <ericgroseclos@hotmail.com>
2018-01-20 15:47:33 +0100

    Initial Commit

---
+++ Quiz App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz App
+//
+//  Created by Eric Groseclos on 2018-01-20.
+//  Copyright © 2018 Eric Groseclos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
